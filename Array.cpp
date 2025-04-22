45d7a44ba79ac508419cc901724fdec98d957361
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-04-22 12:34:33 +0700

    array

--- Array.cpp
+++ Array.cpp
@@ -21,3 +21,13 @@ int main()
         cout << "Masukan nama ke " << i + 1 << " = ";
         cin >> nama[i];
     }
+
+    cout << endl;
+    cout << "Menampilkan Array Nama" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Nama ke " << i + 1 << " = " << nama[i] << endl;
+    }
+}
